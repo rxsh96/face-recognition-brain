89feb300d7912d84b7332f5dbee26e1cf004b9d6
Ricardo Serrano <rxsh964@gmail.com>
2020-03-30 19:29:05 -0500

    Updates

---
+++ precache-manifest.d41701519294237bc22e20f0255dbab1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6adb3e98dabeeb5678532667eee2b9",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "c9cdd1fdd9779542a069",
+    "url": "/face-recognition-brain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "eda840db58759abca74a",
+    "url": "/face-recognition-brain/static/css/main.cabbcc60.chunk.css"
+  },
+  {
+    "revision": "c9cdd1fdd9779542a069",
+    "url": "/face-recognition-brain/static/js/2.fcbdb3d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognition-brain/static/js/2.fcbdb3d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda840db58759abca74a",
+    "url": "/face-recognition-brain/static/js/main.3a26093c.chunk.js"
+  },
+  {
+    "revision": "29e5821f791c973dcb04",
+    "url": "/face-recognition-brain/static/js/runtime-main.e3020e8e.js"
+  }
+]);
